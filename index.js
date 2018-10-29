b8775b113bc3bc79b2df54a0735bb2ebbbdd0264
anjelhm <anjelhm@gmail.com>
2018-10-28 20:12:25 -0600

    Importar componente de comunicaciones

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ import Button from "./src/components/Button";
 
 //ui
 import TripList from "./src/ui/triplist/TripList";
+import CommunicationActions from './src/ui/communicatios/CommunicationActions';
 
 export {
   InputText,
   Button,
   TripList,
+  CommunicationActions,
 };
